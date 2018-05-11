ad8b37fc5a968984d24a321596fedb78059e0b79
shyamsir12 <shyamrahangdale38@gmail.com>
2018-05-11 17:54:25 +0530

    my first commit

--- app/src/main/java/com/example/user3/guideapp/MainActivity.java
+++ app/src/main/java/com/example/user3/guideapp/MainActivity.java
@@ -2,12 +2,51 @@ package com.example.user3.guideapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends  AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar =findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+    @Override
+   public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu ,menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_profile:
+                // User chose the "Settings" item, show the app settings UI...
+                Toast.makeText(this,"profile clicked",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_logout:
+                // User chose the "Settings" item, show the app settings UI...
+                Toast.makeText(this,"logout clicked",Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.action_credits:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                Toast.makeText(this,"credits clicked",Toast.LENGTH_SHORT).show();
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
